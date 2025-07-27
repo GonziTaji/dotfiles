2e8d6df08a335dcdd987e8b26d0db6b6ac9d47e9
yogusita <gonzalo.tajmuch@gmail.com>
2025-07-27 04:04:56 -0400

    nvim config changes

--- .config/nvim/lua/plugins/prettier.lua
+++ .config/nvim/lua/plugins/prettier.lua
@@ -1,12 +1,6 @@
 return {
     "prettier/vim-prettier",
-    config = function ()
+    config = function()
         vim.keymap.set("n", "<leader>fo", ":PrettierAsync<CR>", { desc = "Format document" })
-
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function(args)
-                vim.cmd("Prettier")
-            end
-        })
     end
 }

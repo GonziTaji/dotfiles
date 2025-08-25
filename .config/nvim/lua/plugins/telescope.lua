4474440acd5477577e47344276013f36774fb9b4
Gonzalo Tajmuch <gonzalo.tajmuch@uno.cl>
2025-08-25 12:30:03 -0400

    telescope git keybinds

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,16 +26,30 @@ return {
 
         -- keybinds
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
-        vim.keymap.set('n', '<leader>pk', builtin.keymaps, { desc = 'Telescope keybinds' })
-        vim.keymap.set('n', '<leader>nc', function()
-            require('telescope.builtin').find_files({
+
+        -- utils
+        function find_nvim_config_files()
+            builtin.find_files({
                 prompt_title = "Neovim config files",
                 cwd = vim.fn.stdpath("config"),
             })
-        end, { desc = 'Show nvim config files' })
+        end
+
+        -- configuration info
+        vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>pk', builtin.keymaps, { desc = 'Telescope keybinds' })
+        vim.keymap.set('n', '<leader>nc', find_nvim_config_files, { desc = 'Show nvim config files' })
+        -- workspace navigation
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
+        -- git 
+        vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
+        vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git commits' })
+        vim.keymap.set('n', '<leader>gt', builtin.git_stash, { desc = 'Telescope git stashes' })
+        vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope git branches' })
+        vim.keymap.set('n', '<leader>gf', builtin.git_bcommits, { desc = 'Telescope git commits of current buffer' })
+
+        -- custom
     end
 }
